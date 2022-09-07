10311f65a0a602b3e11162d9cde08eb90c00fb46
Michael <mmoses1127@gmail.com>
2022-09-07 14:44:37 -0700

    create full database

--- import_db.sql
+++ import_db.sql
@@ -19,60 +19,72 @@ DROP TABLE IF EXISTS questions;
     FOREIGN KEY (user_id) REFERENCES users(id)
  );
 
- DROP TABLE IF EXISTS question_follows 
+ DROP TABLE IF EXISTS question_follows;
+
  CREATE TABLE question_follows (
     
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
     FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
  );
 
- DROP TABLE IF EXISTS replies
+ DROP TABLE IF EXISTS replies;
  CREATE TABLE replies (
 
    id INTEGER PRIMARY KEY,
-   FOREIGN KEY (question_id) REFERENCES questions(id),
-   FOREIGN KEY (parent_reply) REFERENCES reply(id),
-   FOREIGN KEY (author) REFERENCES users(id),
+   question_id INTEGER NOT NULL,
+   parent_reply INTEGER,
+   author INTEGER NOT NULL,
    body TEXT,
+
+   FOREIGN KEY (question_id) REFERENCES questions(id),
+   FOREIGN KEY (parent_reply) REFERENCES replies(id),
+   FOREIGN KEY (author) REFERENCES users(id)
+   
  );
  
- DROP TABLE IF EXISTS question_likes
+ DROP TABLE IF EXISTS question_likes;
  CREATE TABLE question_likes (
 
+   question INTEGER NOT NULL,
+   user INTEGER NOT NULL,
+
    FOREIGN KEY (question) REFERENCES questions(id),
-   FOREIGN KEY (user) REFERENCES users(id),
+   FOREIGN KEY (user) REFERENCES users(id)
 
  );
 
 INSERT INTO 
-    users(fname,lname)
+    users (fname,lname)
 VALUES
     ('Shu', 'Shibahara'),
     ('Mike', 'Moses');
 
-INSERT INTO  
-    questions(title, body)
+INSERT INTO
+    questions (title, body, user_id)
 VALUES
-    ('Where''s my hat?', 'Yesterday I was drunk and I lost my hat'),
-    ('Who''s the best teacher?', 'Trying to find a tutor for coding.');
+    ('Where''s my hat?', 'Yesterday I was drunk and I lost my hat', 1),
+    ('Who''s the best teacher?', 'Trying to find a tutor for coding.', 1);
 
 INSERT INTO    
-    question_follows(user_id, question_id)
+    question_follows (user_id, question_id)
 VALUES
-    (1,2)
+    (1,2),
     (1,1);
 
 INSERT INTO 
-    replies(question_id, parent_reply, author, body)
+    replies (question_id, parent_reply, author, body)
 VALUES
-    (1, NULL, 1, 'I should learn to drink less.')
-    (1, 1, 2, 'You learned your lesson punk!')
+    (1, NULL, 1, 'I should learn to drink less.'),
+    (1, 1, 2, 'You learned your lesson punk!');
 
 INSERT INTO
-    question_likes(question,user)
+    question_likes (question,user)
 VALUES
-    (2,2)
-    (1,2)
+    (2,2),
+    (1,2);
 
 
 
