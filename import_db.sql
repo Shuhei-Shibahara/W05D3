bad0b160e063e268643686b7e6b2e51102467bdb
Michael <mmoses1127@gmail.com>
2022-09-07 14:11:07 -0700

    finish creating tables

--- import_db.sql
+++ import_db.sql
@@ -1,6 +1,6 @@
+PRAGMA foreign_keys = ON;
+
 DROP TABLE IF EXISTS users;
-
-
 CREATE TABLE users (
 
     id INTEGER PRIMARY KEY,
@@ -9,7 +9,6 @@ CREATE TABLE users (
 );
 
 DROP TABLE IF EXISTS questions;
-
  CREATE TABLE questions (
 
     id INTEGER PRIMARY KEY,
@@ -20,7 +19,6 @@ DROP TABLE IF EXISTS questions;
  );
 
  DROP TABLE IF EXISTS question_follows 
-
  CREATE TABLE question_follows (
     
     FOREIGN KEY (user_id) REFERENCES users(id),
@@ -28,9 +26,19 @@ DROP TABLE IF EXISTS questions;
  )
 
  DROP TABLE IF EXISTS replies
-
  CREATE TABLE replies (
 
-    
+   id INTEGER PRIMARY KEY,
+   FOREIGN KEY (question_id) REFERENCES questions(id),
+   FOREIGN KEY (parent_reply) REFERENCES reply(id),
+   FOREIGN KEY (author) REFERENCES users(id)
+   FOREIGN KEY (body) REFERENCES questions(body)
  )
  
+ DROP TABLE IF EXISTS question_likes
+ CREATE TABLE question_likes (
+
+   FOREIGN KEY (question) REFERENCES questions(id),
+   FOREIGN KEY (user) REFERENCES users(id)
+
+ )
