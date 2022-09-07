15d66c161beaef93cf35408ebe31f760292af1d1
Michael <mmoses1127@gmail.com>
2022-09-07 16:02:02 -0700

    finish call_by_id

--- questions_database.rb
+++ questions_database.rb
@@ -12,6 +12,7 @@ class QuestionsDatabase < SQLite3::Database
 end
 
 class Users
+    attr_accessor :id, :fname, :lname
 
     def initialize(options)
         @id = options['id']
@@ -20,8 +21,8 @@ class Users
     end
 
     def self.find_by_id(id)
-        Users.new(
-        QuestionsDatabase.instance.execute(<<- SQL, id)
+        user =
+        QuestionsDatabase.instance.execute(<<-SQL, id)
         SELECT
             *
         FROM  
@@ -29,6 +30,60 @@ class Users
         WHERE
             id = ?
         SQL
-        )
+        return nil unless user.length > 0
+        Users.new(user.first)
+    end
+end
+
+
+class Questions
+    attr_accessor :id, :body, :user_id, :id
+
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @user_id = options['user_id']
+    end
+
+    def self.find_by_id(id)
+        question = 
+        QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT
+            *
+        FROM  
+            questions
+        WHERE
+            id = ?
+        SQL
+        return nil unless question.length > 0
+        Questions.new(question.first)
+    end
+end
+
+
+class Replies
+    attr_accessor :id, :question_id, :parent_reply, :author, :body
+
+    def initialize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @parent_reply = options['parent_reply']
+        @author = options['author']
+        @body = options['body']
+    end
+
+    def self.find_by_id(id)
+        reply = 
+        QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT
+            *
+        FROM  
+            replies
+        WHERE
+            id = ?
+        SQL
+        return nil unless reply.length > 0
+        Replies.new(reply.first)
     end
 end
